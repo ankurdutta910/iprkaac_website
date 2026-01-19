c5272e33da52f4ccdfdb00aa01944b8a56c83350
ANKUR JYOTI DUTTA <103182503+ankurdutta910@users.noreply.github.com>
2026-01-19 20:09:52 +0530

    Update Supabase.js

--- src/Supabase.js
+++ src/Supabase.js
@@ -1,8 +1,9 @@
 // src/supabaseClient.js
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://eijrzxqqqhucpplebrxd.supabase.co";
+const supabaseUrl = "***********************************************************************";
 const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpanJ6eHFxcWh1Y3BwbGVicnhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcxNzMxMTMsImV4cCI6MjA4Mjc0OTExM30.oAfge9cQJFgS9eStrbqno3nYQZ1wELRnQi0UFKkJYzg";
+  "*******************************************************************************************";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
